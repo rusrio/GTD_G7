8d1f925e696656d0dea99b9480473a00adfc6799
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-02 14:09:49 +0200

    arreglada configuracion thymeleaf

--- spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
+++ spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
@@ -1,23 +1,38 @@
 package com.capgemini.presentation;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.capgemini.bussines.servicio.ServicioUser;
+import com.capgemini.bussines.servicioImpl.ServicioUserImpl;
+import com.capgemini.modelo.CategoryVO;
+import com.capgemini.modelo.TaskVO;
+import com.capgemini.modelo.UserVO;
+import com.capgemini.modelo.UserVO.UserStatus;
 
 
 @Controller
 public class HomeController {
+	
+	ServicioUser su = new ServicioUserImpl();
+	
 
 
-	@RequestMapping(value = "/")
-	public ModelAndView login() {
 
-		ModelAndView mv = new ModelAndView();
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String inicio(Model modelo) {
 		
-		//a rellenar
-
-		return mv;
+		su.insertar(new UserVO("paco@paco.mail", true, "paquito-priulero", "pwd-segura", UserStatus.ENABLED,
+				new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+		su.insertar(new UserVO("paco@paco.mail", true, "paquito-priulero-2", "pwd-segura", UserStatus.ENABLED,
+				new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+		
+		modelo.addAttribute("usuario", su.findAll());
+		return "ppal";
 
 	}
 
