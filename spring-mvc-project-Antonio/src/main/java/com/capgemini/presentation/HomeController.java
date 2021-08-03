4202e7e3da957b6a414e0df8da1a71f9cd084a25
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-03 19:20:16 +0200

    implementado login para admin y normal user

--- spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
+++ spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.capgemini.bussines.servicio.ServicioUser;
 import com.capgemini.bussines.servicioImpl.ServicioUserImpl;
@@ -14,24 +15,49 @@ import com.capgemini.modelo.TaskVO;
 import com.capgemini.modelo.UserVO;
 import com.capgemini.modelo.UserVO.UserStatus;
 
-
 @Controller
 public class HomeController {
-	
+
 	ServicioUser su = new ServicioUserImpl();
-	
 
-
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String inicio(Model modelo) {
+	@GetMapping("/")
+	public String login() {
 		
-		su.insertar(new UserVO("sr_capgemini@capgemini.com", true, "master_of_the_universe", "12345", UserStatus.ENABLED,
-				new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
-		su.insertar(new UserVO("adam_responde@capgemini.com", false, "centralita123", "adam_the_best", UserStatus.DISABLED,
-				new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+		if (su.findAll().size() != 4) {//ELIMINAR, previo para añadir datos
+			su.insertar(new UserVO("sr_capgemini@capgemini.com", true, "root", "root",
+					UserStatus.ENABLED, new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+			su.insertar(new UserVO("adam_responde@capgemini.com", false, "normal", "normal",
+					UserStatus.DISABLED, new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+		}
+		
+		return "login";
+	}
+
+	@PostMapping("/validaUser")
+	public String validaUser(@RequestParam String login, @RequestParam String pwd, Model modelo) {
+		
+		UserVO user = su.findByLogin(login);//creamos el usuario buscandolo por el login
+		
+		if(user==null) {//si no podemos encontrar el usuario, es que el login está mal
+			return "login";
+		}
+
+		if (user.getPassword().equals(pwd)) {//comprobamos que, exisitendo el login, el pwd es correcto
+			if (user.isAdmin()) {//si es admin, vamos a su pagina
+				return "redirect:ppal";
+			} else {//si es un user normal, a la que le corresponda
+				return "user";
+			}
+		}else {//si la pwd no es correcta, volvemos a pedir que ingrese credenciales
+			return "login";
+		}
+	}
+
+	@GetMapping("/ppal")
+	public String ppal(Model modelo) {
 		
 		modelo.addAttribute("usuario", su.findAll());
+
 		return "ppal";
 
 	}
