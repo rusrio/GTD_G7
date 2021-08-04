94c5b72e92a3e42325b03f8731a1f322659453d0
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-04 10:40:01 +0200

    añadido eliminar-activar user desde admin controller

--- spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
+++ spring-mvc-project-Antonio/src/main/java/com/capgemini/presentation/HomeController.java
@@ -20,51 +20,81 @@ public class HomeController {
 
 	ServicioUser su = new ServicioUserImpl();
 
+	// creamos un metodo para que nos cree al principio el usuario admin y algun
+	// otro usuario
+
+	public void inicio() {
+		
+		if (su.findById(1) == null) {
+			su.insertar(new UserVO("root@capgemini.com", true, "root", "root", UserStatus.ENABLED,
+					new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+
+			su.insertar(new UserVO("usuario1@capgemini.com", false, "usuario1", "usuario1", UserStatus.ENABLED,
+					new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+
+			su.insertar(new UserVO("usuario2@capgemini.com", false, "usuario2", "usuario2", UserStatus.ENABLED,
+					new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+
+			su.insertar(new UserVO("usuario3@capgemini.com", false, "usuario3", "usuario3", UserStatus.DISABLED,
+					new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+
+			su.insertar(new UserVO("usuario4@capgemini.com", false, "usuario4", "usuario4", UserStatus.DISABLED,
+					new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
+		}
+	}
+
 	@GetMapping("/")
 	public String login() {
-		
-		if (su.findAll().size() != 4) {//ELIMINAR, previo para añadir datos
-			su.insertar(new UserVO("sr_capgemini@capgemini.com", true, "root", "root",
-					UserStatus.ENABLED, new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
-			su.insertar(new UserVO("adam_responde@capgemini.com", false, "normal", "normal",
-					UserStatus.DISABLED, new ArrayList<CategoryVO>(), new ArrayList<TaskVO>()));
-		}
-		
+
+		inicio();
+
 		return "login";
 	}
 
 	@PostMapping("/validaUser")
 	public String validaUser(@RequestParam String login, @RequestParam String pwd, Model modelo) {
-		
-		UserVO user = su.findByLogin(login);//creamos el usuario buscandolo por el login
-		
-		if(user==null) {//si no podemos encontrar el usuario, es que el login está mal
+
+		UserVO user = su.findByLogin(login);// creamos el usuario buscandolo por el login
+
+		if (user == null) {// si no podemos encontrar el usuario, es que el login está mal
 			return "login";
 		}
 
-		if (user.getPassword().equals(pwd)) {//comprobamos que, exisitendo el login, el pwd es correcto
-			if (user.isAdmin()) {//si es admin, vamos a su pagina
+		if (user.getPassword().equals(pwd)) {// comprobamos que, exisitendo el login, el pwd es correcto
+			if (user.isAdmin()) {// si es admin, vamos a su pagina
 				return "redirect:ppal";
-			} else {//si es un user normal, a la que le corresponda
+			} else {// si es un user normal, a la que le corresponda
 				return "user";
 			}
-		}else {//si la pwd no es correcta, volvemos a pedir que ingrese credenciales
+		} else {// si la pwd no es correcta, volvemos a pedir que ingrese credenciales
 			return "login";
 		}
 	}
 
 	@GetMapping("/ppal")
 	public String ppal(Model modelo) {
-		
+
 		modelo.addAttribute("usuario", su.findAll());
 
 		return "ppal";
 
 	}
-	
+
 	@GetMapping("/deleteuser")
 	public String deleteUser(@RequestParam int iduser) {
 		su.eliminar(su.findById(iduser));
+		return "redirect:ppal";
+	}
+	
+	@GetMapping("/activeuser")
+	public String activeUser(@RequestParam int iduser) {
+		UserVO user=su.findById(iduser);
+		if(user.getStatus()==UserStatus.DISABLED) {
+			user.setStatus(UserStatus.ENABLED);
+		}else {
+			user.setStatus(UserStatus.DISABLED);
+		}
+		
 		return "redirect:ppal";
 	}
 
